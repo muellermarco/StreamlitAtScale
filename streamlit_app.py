e1a4e7a08cc7b2e16658fb48bca81e85241d357d
muellermarco <104429125+muellermarco@users.noreply.github.com>
2024-03-20 13:56:27 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import streamlit as st
 from atscale.client import Client
 from atscale.data_model import DataModel
 from atscale.project import Project
+import pandas as pd
 
-st.write("My first App")
-
+st.title("AtScale python API Demo")
+#st.secrets["atscale_host"]
 client = Client(server=st.secrets["atscale_host"],
                 username=st.secrets["atscale_user"],
                 password=st.secrets["atscale_password"],
@@ -15,10 +16,23 @@ test = client.connect()
 
 #Sales Insights
 project:Project = client.select_project(published_project_id='3d965074-0e49-42df-4151-74541f019bd0',draft_project_id='2e0203d7-fa65-4c28-7b65-357eb4aee0ea')
-data_model = project.select_data_model("b89a2fb7-74f4-4828-706e-70f7186e10a0")
+data_model:DataModel = project.select_data_model("b89a2fb7-74f4-4828-706e-70f7186e10a0")
 
-features_cat = data_model.get_all_categorical_feature_names()
+st.header("Ordered Quantity by Color")
 
-st.write(features_cat)
+bar_cart_data = data_model.get_data(['orderquantity1','CountryCity'])
 
-st.write("new")
+st.bar_chart(data=bar_cart_data, x="CountryCity", y="orderquantity1")
+
+line_cart_data = data_model.get_data(['orderquantity1','Order DayMonth'])
+
+st.line_chart(data=line_cart_data,x="Order DayMonth", y="orderquantity1")
+
+dimensions = pd.DataFrame(data_model.get_all_categorical_feature_names())
+
+dimensions
+
+measures = pd.DataFrame(data_model.get_all_numeric_feature_names())
+
+measures
+
