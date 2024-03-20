72753776bb2b781b7d2fbe6813f1d6ad654a2a22
muellermarco <104429125+muellermarco@users.noreply.github.com>
2024-03-20 12:18:52 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ from atscale.data_model import DataModel
 from atscale.project import Project
 
 st.write("My first App")
-client = Client()
-#client = Client(server=st.secrets["atscale_host"],
-#                username=st.secrets["atscale_user"],
-#                password=st.secrets["atscale_password"],
-#                organization='default'
-#               )
+
+client = Client(server=st.secrets["atscale_host"],
+                username=st.secrets["atscale_user"],
+                password=st.secrets["atscale_password"],
+                organization='default'
+               )
 
 #test = client.connect()
