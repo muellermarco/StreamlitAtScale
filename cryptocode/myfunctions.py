2ee28079704aba6ca87dd5b539f7027d0f995457
muellermarco <104429125+muellermarco@users.noreply.github.com>
2024-03-20 11:18:23 +0100

    rename imports

--- cryptocode/myfunctions.py
+++ cryptocode/myfunctions.py
@@ -1,7 +1,7 @@
 from base64 import b64encode, b64decode
 import hashlib
-from Cryptodome.Cipher import AES
-from Cryptodome.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 import time
 def encrypt(message , password):
     plain_text = message
