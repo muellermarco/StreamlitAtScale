d1f071007b953a26fd47d20ce49d941437be15f0
muellermarco <104429125+muellermarco@users.noreply.github.com>
2024-03-20 12:26:45 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,11 @@ client = Client(server=st.secrets["atscale_host"],
                )
 
 test = client.connect()
+
+#Sales Insights
+project:Project = client.select_project(published_project_id='3d965074-0e49-42df-4151-74541f019bd0',draft_project_id='2e0203d7-fa65-4c28-7b65-357eb4aee0ea')
+data_model = project.select_data_model("b89a2fb7-74f4-4828-706e-70f7186e10a0")
+
+features_cat = data_model.get_all_categorical_feature_names()
+
+st.write(features_cat)
